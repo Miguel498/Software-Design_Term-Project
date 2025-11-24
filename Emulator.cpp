6d12e3903967532d79c3d1664f38e9db185a0ab6
Miguel498 <miguelsanta2004@gmail.com>
2025-11-24 12:37:55 -0500

    Correct add operation in opcode switch case
    Fix the add operation in the emulator switch case.

--- Emulator.cpp
+++ Emulator.cpp
@@ -9,5 +9,17 @@
             int opcode = (int) (inst / 10'000'000'000LL); // to show the op code
             int address1 = (inst / 100'000) % 10'000; // to show the first address 
             int address2 = inst % 100'000; // to show the second address
+
+            switch( opcode )
+             {
+              case 1: //add
+                 m_memory[address1] = += m_memory[address2];
+                 loc++;
+              case 2: //-=
+
+              default:
+                 // Error
+             }
+         
         }
     }
