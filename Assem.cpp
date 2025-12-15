7c20c2aac6ec25cd5fb7d967a46383576f23235f
Miguel498 <miguelsanta2004@gmail.com>
2025-12-15 14:42:51 -0500

    Clean up main function in Assem.cpp
    Refactor main function for improved readability and error handling.

--- Assem.cpp
+++ Assem.cpp
@@ -1,27 +1,37 @@
 /*
  * Assembler main program.
  */
-#include "stdafx.h"     // This must be present if you use precompiled headers which you will use. 
+#include "stdafx.h"
 #include <stdio.h>
 
 #include "Assembler.h"
+#include "Errors.h"
 
-int main( int argc, char *argv[] )
+int main(int argc, char* argv[])
 {
-    Assembler assem( argc, argv );
+    Assembler assem(argc, argv);
+
+    // Initialize error reporting.
+    Errors::InitErrorReporting();
 
     // Establish the location of the labels:
-    assem.PassI( );
+    assem.PassI();
 
     // Display the symbol table.
-    //MIGGY assem.DisplaySymbolTable();
+    assem.DisplaySymbolTable();
 
     // Finish the translation and output it to the screen.
-    //MIGGY assem.PassII( );
-    
+    assem.PassII();
+
     // Run the emulator on the translation of the assembler language program that was generated in Pass II.
-    //MIGGY assem.RunProgramInEmulator();
-   
+    assem.RunProgramInEmulator();
+
+    // Display any errors that were recorded during assembly.
+    if (Errors::HasErrors()) {
+        cout << "Errors detected during assembly:" << endl;
+        Errors::DisplayErrors();
+    }
+
     // Terminate indicating all is well.  If there is an unrecoverable error, the 
     // program will terminate at the point that it occurred with an exit(1) call.
     return 0;
