5ec66a57e890b18bf87b66313481c6e367641245
Miguel498 <miguelsanta2004@gmail.com>
2025-12-15 13:32:07 -0500

    Refactor stdafx.h for standard library usage
    Updated stdafx.h to use standard libraries and added conditional Windows includes.

--- stdafx.h
+++ stdafx.h
@@ -1,24 +1,19 @@
-// stdafx.h : include file for standard system include files,
-// or project specific include files that are used frequently, but
-// are changed infrequently
-//
+// Cross-platform stdafx.h (precompiled header alternative)
 
 #pragma once
 
-#define WIN32_LEAN_AND_MEAN		// Exclude rarely-used stuff from Windows headers
-#include <stdio.h>
-#include <tchar.h>
-
-
-
-// TODO: reference additional headers your program requires here
-// Standard include files.
-#include <stdio.h>
+// Standard libraries
 #include <iostream>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string>
-#include <windows.h>
 #include <map>
-#include<istream>
+#include <vector>
+
+// If compiling on Windows
+#ifdef _WIN32
+    #define WIN32_LEAN_AND_MEAN
+    #include <windows.h>
+#endif
 
 using namespace std;
